1f808f0a4095787b31d54ce4a1a17f25dcdfc1da
mash3133 <maesha3133@gmail.com>
2025-01-13 21:05:32 -0500

    the ui is done, time for backend

--- react-gpt/src/App.jsx
+++ react-gpt/src/App.jsx
@@ -1,7 +1,80 @@
 
 import './App.css'
+import React, {useState} from 'react';
 
 function App() {
+  const [courseName, setCourseName] = useState("");
+  const [typeOfAssignment, setTypeOfAssignment] = useState("");
+  const [grading, setGrading] = useState("");
+
+  const resetValues = () => {
+    setCourseName("");
+    setGrading("");
+    setTypeOfAssignment("");
+  }
+
+
+
+  return(
+    <div>
+      <form>
+      {/* Used this for input: https://www.dhiwise.com/post/a-step-by-step-guide-to-retrieving-input-values-in-react */}
+        <label>
+          Course Name:
+          <input
+            type="text"
+            value={courseName}
+            onChange={event => {
+              setCourseName(event.target.value);
+            }}
+          />
+        </label>
+        {/* Used this for select: https://react.dev/reference/react-dom/components/select  */}
+        <label>
+          Type of Assignment:
+          <select
+            value={typeOfAssignment}
+            onChange={(e)=>{
+              setTypeOfAssignment(e.target.value);
+            }}
+          >
+            <option value="essay">Essay</option>
+            <option value="presentation">Presentation</option>
+            <option value="test">Test</option>
+          </select>
+        </label>
+        <label>
+          {/* Used this for the radio buttons: https://www.pluralsight.com/resources/blog/guides/how-to-use-radio-buttons-in-reactjs */}
+          Grading:
+            <div onChange={(e)=>{
+                setGrading(e.target.value);
+              }}>
+              <input 
+                type="radio" 
+                value="clarity"
+                name="grading"
+                
+              />Clarity
+              <input 
+                type="radio" 
+                value="creativity"
+                name="grading"
+
+              />Creativity
+              <input 
+                type="radio" 
+                value="technical accuracy"
+                name="grading"
+
+              />Technical Accuracy
+            </div>
+        </label>
+      </form>
+      <button onClick={resetValues}>Reset</button>
+    </div>
+  )
+
+
 }
 
 export default App
